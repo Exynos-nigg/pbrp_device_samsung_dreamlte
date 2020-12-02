ce0407de29e6de034b8858ff61058f1e80029a8f
bruh™ <69905300+Exynos-nigg@users.noreply.github.com>
2020-12-02 11:00:05 +0400

    Omni_dreamlte : add pbrp vars

--- omni_dreamlte.mk
+++ omni_dreamlte.mk
@@ -20,6 +20,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/pb/config/common.mk)
 
+# PBRP specific vars
+# Because our device is arm64
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# dalvik optimization
+$(call inherit-product, $(SRC_TARGET_DIR)/product/runtime_libart.mk)
+
 PRODUCT_DEVICE := dreamlte
 PRODUCT_NAME := omni_dreamlte
 PRODUCT_MODEL := Galaxy S8
